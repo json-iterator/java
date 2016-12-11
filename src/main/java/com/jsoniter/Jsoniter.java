56457290e33635284258ddbb34dd5e14ba7c8aa9
Tao Wen <taowen@gmail.com>
2016-12-11 13:16:48 +0800

    implement base64

--- src/main/java/com/jsoniter/Jsoniter.java
+++ src/main/java/com/jsoniter/Jsoniter.java
@@ -1,5 +1,6 @@
 package com.jsoniter;
 
+import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,6 +13,22 @@ public class Jsoniter implements Closeable {
 
     final static int[] digits = new int[256];
     final static ValueType[] valueTypes = new ValueType[256];
+    int[] base64Tbl = {
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63, 52, 53, 54,
+            55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2,
+            3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+            20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30,
+            31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+            48, 49, 50, 51, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
     InputStream in;
     byte[] buf;
     int head;
@@ -375,8 +392,47 @@ public class Jsoniter implements Closeable {
     }
 
     public final byte[] readBase64() throws IOException {
+        // from https://gist.github.com/EmilHernvall/953733
         Slice slice = readSlice();
-        return null;
+        if (slice == null) {
+            return null;
+        }
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        int end = slice.head + slice.len;
+        for (int i = slice.head; i < end; i++) {
+            int b = 0;
+            if (base64Tbl[slice.data[i]] != -1) {
+                b = (base64Tbl[slice.data[i]] & 0xFF) << 18;
+            }
+            // skip unknown characters
+            else {
+                i++;
+                continue;
+            }
+
+            int num = 0;
+            if (i + 1 < end && base64Tbl[slice.data[i+1]] != -1) {
+                b = b | ((base64Tbl[slice.data[i+1]] & 0xFF) << 12);
+                num++;
+            }
+            if (i + 2 < end && base64Tbl[slice.data[i+2]] != -1) {
+                b = b | ((base64Tbl[slice.data[i+2]] & 0xFF) << 6);
+                num++;
+            }
+            if (i + 3 < end && base64Tbl[slice.data[i+3]] != -1) {
+                b = b | (base64Tbl[slice.data[i+3]] & 0xFF);
+                num++;
+            }
+
+            while (num > 0) {
+                int c = (b & 0xFF0000) >> 16;
+                buffer.write((char)c);
+                b <<= 8;
+                num--;
+            }
+            i += 4;
+        }
+        return buffer.toByteArray();
     }
 
     public final String readString() throws IOException {
