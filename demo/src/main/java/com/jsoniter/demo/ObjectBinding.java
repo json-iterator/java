a151b58f807e922a703121d1b865f5848b120e67
Tao Wen <taowen@gmail.com>
2016-12-19 09:38:30 +0800

    update demo

---
+++ demo/src/main/java/com/jsoniter/demo/ObjectBinding.java
@@ -0,0 +1,137 @@
+package com.jsoniter.demo;
+
+import com.dslplatform.json.CompiledJson;
+import com.dslplatform.json.DslJson;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
+import com.jsoniter.JsonIterator;
+import com.jsoniter.spi.TypeLiteral;
+import org.junit.Test;
+import org.openjdk.jmh.Main;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.io.IOException;
+
+@State(Scope.Thread)
+public class ObjectBinding {
+
+    private TypeLiteral<TestObject> typeLiteral;
+    private ObjectMapper jackson;
+    private byte[] input;
+    private TypeReference<TestObject> typeRef;
+    private DslJson dslJson;
+    private Class<TestObject> clazz;
+
+    @CompiledJson
+    public static class TestObject {
+        public int field1;
+        public int field2;
+
+        @Override
+        public String toString() {
+            return "TestObject{" +
+                    "field1=" + field1 +
+                    ", field2=" + field2 +
+                    '}';
+        }
+    }
+
+
+    private JsonIterator iter;
+
+    @Setup(Level.Trial)
+    public void benchSetup() {
+        input = "{'field1':100,'field2':101}".replace('\'', '"').getBytes();
+        iter = JsonIterator.parse(input);
+        typeLiteral = new TypeLiteral<TestObject>() {
+        };
+        typeRef = new TypeReference<TestObject>() {
+        };
+        clazz = TestObject.class;
+        jackson = new ObjectMapper();
+        jackson.registerModule(new AfterburnerModule());
+        dslJson = new DslJson();
+    }
+
+    @Test
+    public void test() throws IOException {
+        benchSetup();
+        System.out.println(withIterator());
+        System.out.println(withBindApiNoneStrictMode());
+        System.out.println(withBindApiStrictMode());
+        System.out.println(withJackson());
+        System.out.println(withDsljson());
+    }
+
+    @Benchmark
+    public void withIterator(Blackhole bh) throws IOException {
+        bh.consume(withIterator());
+    }
+
+    @Benchmark
+    public void withBindApiNoneStrictMode(Blackhole bh) throws IOException {
+        bh.consume(withBindApiNoneStrictMode());
+    }
+
+    @Benchmark
+    public void withBindApiStrictMode(Blackhole bh) throws IOException {
+        bh.consume(withBindApiStrictMode());
+    }
+
+    @Benchmark
+    public void withJackson(Blackhole bh) throws IOException {
+        bh.consume(withJackson());
+    }
+
+    @Benchmark
+    public void withDsljson(Blackhole bh) throws IOException {
+        bh.consume(withDsljson());
+    }
+
+    public static void main(String[] args) throws Exception {
+        Main.main(new String[]{
+                "-i", "5",
+                "-wi", "5",
+                "-f", "1"
+        });
+    }
+
+    private TestObject withIterator() throws IOException {
+        iter.reset();
+        TestObject obj = new TestObject();
+        for (String field = iter.readObject(); field != null; field = iter.readObject()) {
+            switch (field) {
+                case "field1":
+                    obj.field1 = iter.readInt();
+                    continue;
+                case "field2":
+                    obj.field2 = iter.readInt();
+                    continue;
+                default:
+                    iter.skip();
+            }
+        }
+        return obj;
+    }
+
+    private TestObject withBindApiNoneStrictMode() throws IOException {
+        iter.reset();
+        return iter.read(typeLiteral);
+    }
+
+    private TestObject withBindApiStrictMode() throws IOException {
+        JsonIterator.enableStrictMode();
+        iter.reset();
+        return iter.read(typeLiteral);
+    }
+
+    private TestObject withJackson() throws IOException {
+        return jackson.readValue(input, typeRef);
+    }
+
+    private TestObject withDsljson() throws IOException {
+        return (TestObject) dslJson.deserialize(clazz, input, input.length);
+    }
+}
--- demo/src/main/java/com/jsoniter/demo/SimpleCase.java
+++
@@ -1,7 +0,0 @@
-package com.jsoniter.demo;
-
-public class SimpleCase {
-    public static void main(String[] args) {
-
-    }
-}
