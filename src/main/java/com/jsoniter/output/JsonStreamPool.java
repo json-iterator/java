7cdbed67653c37a06fe4d5678c26c83ed75557c3
Tao Wen <taowen@gmail.com>
2017-06-24 14:32:34 +0800

    remove unused code

--- src/main/java/com/jsoniter/output/JsonStreamPool.java
+++ src/main/java/com/jsoniter/output/JsonStreamPool.java
@@ -1,15 +1,9 @@
 package com.jsoniter.output;
 
-import java.io.ByteArrayOutputStream;
-
 public class JsonStreamPool {
 
     private final static ThreadLocal<JsonStream> slot1 = new ThreadLocal<JsonStream>();
     private final static ThreadLocal<JsonStream> slot2 = new ThreadLocal<JsonStream>();
-    private final static ThreadLocal<AsciiOutputStream> osSlot1 = new ThreadLocal<AsciiOutputStream>();
-    private final static ThreadLocal<AsciiOutputStream> osSlot2 = new ThreadLocal<AsciiOutputStream>();
-    private final static ThreadLocal<ByteArrayOutputStream> baosSlot1 = new ThreadLocal<ByteArrayOutputStream>();
-    private final static ThreadLocal<ByteArrayOutputStream> baosSlot2 = new ThreadLocal<ByteArrayOutputStream>();
 
     public static JsonStream borrowJsonStream() {
         JsonStream stream = slot1.get();
@@ -38,53 +32,4 @@ public class JsonStreamPool {
         }
     }
 
-    public static AsciiOutputStream borrowAsciiOutputStream() {
-        AsciiOutputStream stream = osSlot1.get();
-        if (stream != null) {
-            osSlot1.set(null);
-            return stream;
-        }
-        stream = osSlot2.get();
-        if (stream != null) {
-            osSlot2.set(null);
-            return stream;
-        }
-        return new AsciiOutputStream();
-    }
-
-    public static void returnAsciiOutputStream(AsciiOutputStream asciiOutputStream) {
-        if (osSlot1.get() == null) {
-            osSlot1.set(asciiOutputStream);
-            return;
-        }
-        if (osSlot2.get() == null) {
-            osSlot2.set(asciiOutputStream);
-            return;
-        }
-    }
-
-    public static ByteArrayOutputStream borrowByteArrayOutputStream() {
-        ByteArrayOutputStream stream = baosSlot1.get();
-        if (stream != null) {
-            osSlot1.set(null);
-            return stream;
-        }
-        stream = baosSlot2.get();
-        if (stream != null) {
-            osSlot2.set(null);
-            return stream;
-        }
-        return new ByteArrayOutputStream();
-    }
-
-    public static void returnByteArrayOutputStream(ByteArrayOutputStream baos) {
-        if (baosSlot1.get() == null) {
-            baosSlot1.set(baos);
-            return;
-        }
-        if (baosSlot2.get() == null) {
-            baosSlot2.set(baos);
-            return;
-        }
-    }
 }
