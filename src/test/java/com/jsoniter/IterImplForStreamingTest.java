4504c2e0d63d648f40f7f41867b6f91de3e25e7a
Lukas Svoboda <lukas.svoboda@hotmail.com>
2019-06-15 14:42:25 +0200

    Added failing test of loadMore function simulating malfunction mentioned in previous commit.

--- src/test/java/com/jsoniter/IterImplForStreamingTest.java
+++ src/test/java/com/jsoniter/IterImplForStreamingTest.java
@@ -1,6 +1,10 @@
 package com.jsoniter;
 
+import com.jsoniter.any.Any;
+import java.io.IOException;
+import java.io.InputStream;
 import junit.framework.TestCase;
+import org.junit.experimental.categories.Category;
 
 public class IterImplForStreamingTest extends TestCase {
 
@@ -10,5 +14,38 @@ public class IterImplForStreamingTest extends TestCase {
 		IterImplForStreaming.numberChars numberChars = IterImplForStreaming.readNumber(iter);
 		String number = new String(numberChars.chars, 0, numberChars.charsLength);
 		assertEquals(maxDouble, number);
+	}
+
+	@Category(StreamingCategory.class)
+	public void testLoadMore() throws IOException {
+		final String originalContent = "1234";
+		final byte[] src = ("{\"a\":\"" + originalContent + "\"}").getBytes();
+		InputStream slowStream = new InputStream() {
+			int position = 0;
+			boolean pretendEmptyNextRead = false;
+
+			@Override
+			public int read() throws IOException {
+				if (position < src.length) {
+					if (pretendEmptyNextRead) {
+						pretendEmptyNextRead = false;
+						return -1;
+					} else {
+						pretendEmptyNextRead = true;
+						return src[position++];
+					}
+				}
+				return -1;
+			}
+		};
+
+		// Input must definitely fit into such large buffer
+		final int initialBufferSize = src.length * 2;
+		JsonIterator jsonIterator = JsonIterator.parse(slowStream, initialBufferSize);
+		jsonIterator.readObject();
+		Any parsedString = jsonIterator.readAny();
+		assertEquals(originalContent, parsedString.toString());
+		// Check buffer was not expanded prematurely
+		assertEquals(initialBufferSize, jsonIterator.buf.length);
 	}
 }
